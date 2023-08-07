2dff3b961fdb6cd76830b66e1600ab9f9e1558a6
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2023-08-07 23:42:56 +0530

    enhance game over experience

--- client/src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import GameSlice from './Features/GameSlice/GameSlice'
-
-export const store = configureStore({
-	reducer: {
-		game: GameSlice,
-	},
-})
