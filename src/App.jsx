74cde1e5c4116c0a42302e7e6e7a012dad22f7b0
Suryanarayan Bhat <87333995+SHaDeX0@users.noreply.github.com>
2023-08-08 10:09:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Register from './components/Register/Register'
 const App = () => {
 	const router = createBrowserRouter([
 		{ path: '/tic-tac-toe', element: <Home /> },
-                { path: '/tic-tac-toe/register', element: <Register /> },
-                // { path: '*', element: <NotFound /> },
+    { path: '/tic-tac-toe/register', element: <Register /> },
+		// { path: '*', element: <NotFound /> },
 	])
 	return <RouterProvider router={router} />
 }
